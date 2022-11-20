9266685a8ad48057fa824323fc922c91b044e4f5
Carlos Araujo <infocarlosaraujo@gmail.com>
2022-11-19 21:56:49 -0300

    Add code chart_01_x

--- chart_01_x/script.js
+++ chart_01_x/script.js
@@ -1,23 +1,13 @@
-d3.csv('../data/data2000.csv', d3.autoType)
-.then(data => {
-  console.log(data)
-
+d3.csv("../data/data2000.csv", d3.autoType).then((data) => {
+  console.log(data);
+  // Guardamos el svg generado en la variable chart
   let chart = Plot.plot({
-  marks: [
-    Plot.dot(data, {
-      x: "fertility",
-      stroke:"#666af0",
-    }),
-  ],
-  x : {
-    line:true,
-    nice:true,
-    domain : [0, d3.max(data, d => d.fertility)],
-    grid:true
-  },
-  marginLeft: 120,
-});
+    marks: [
+      Plot.dot(data, {
+        x: "fertility",
+      }),
+    ],
+  });
+  // Agregamos chart al div#chart de index.html
   d3.select("#chart").append(() => chart);
 });
-
-
