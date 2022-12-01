a7766140800805467342d4dfdbd51dea212f6ae7
Juli Romero <juliiromero2@gmail.com>
2022-12-01 16:03:34 -0300

    Chart_10_square y Chart_11_tick_mark

---
+++ chart_11_tick_mark/script.js
@@ -0,0 +1,32 @@
+d3.csv("../data/data2000.csv", d3.autoType).then((data) => {
+  createChart(data)
+});
+
+function createChart(data){
+  cluster = [0,1,2,3,4,5]
+  let chart = Plot.plot({
+    height:200,
+    line: true,
+    nice: true,
+    x: {
+      tickFormat: d3.format(".0f"),
+      domain: [0, d3.max(data, (d) => d.fertility)],
+      grid: true,
+    },
+    y: {
+      tickFormat: d3.format(".0f"),
+      domain: cluster,
+      ticks: 5,
+    },
+    marks: [
+      Plot.tickX(data, {
+          x: "fertility", 
+          y: "cluster",
+          stroke:"#666af0",
+          strokeWidth: 1,
+        }) 
+    ],
+  });
+  d3.select("#chart").append(() => chart);
+};
+  
