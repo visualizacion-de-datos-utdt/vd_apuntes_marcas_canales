b6533c259977a7165a0ef879aa6455a5d780a130
Carlos Araujo <infocarlosaraujo@gmail.com>
2022-12-11 23:32:03 -0300

    Avances traduccion

--- chart_02_y/script.js
+++ chart_02_y/script.js
@@ -6,22 +6,11 @@ d3.csv("data2000.csv", d3.autoType).then((data) => {
       Plot.dot(data, {
         x: "fertility",
         y: "cluster",
-        stroke: "#666af0",
       }),
     ],
-    x: {
-      domain: d3.extent(data, (d) => d.fertility),
-      line: true,
-      grid: true,
-    },
     y: {
-      reverse: true,
-      tickFormat: d3.format(".0f"),
-      domain: d3.extent(data, (d) => d.cluster),
-      ticks: 5,
-      line: true,
+      type: "point",
     },
-    color: {},
   });
   d3.select("#chart").append(() => chart);
 });
