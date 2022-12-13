7745dea51cdc084856456341220d4f51f5679324
araujo-carlos <infocarlosaraujo@gmail.com>
2022-12-13 03:28:00 -0300

    Fin tamanio

--- chart_05_size_b/script.js
+++ chart_05_size_b/script.js
@@ -2,27 +2,12 @@ d3.csv("data2000.csv", d3.autoType).then((data) => {
   console.log(data);
 
   let chart = Plot.plot({
-    grid: true,
-    line: true,
+    marks: [Plot.dot(data, { x: "fertility", y: "life_expect", r: "pop" })],
+    r: { range: [0, 50] },
     nice: true,
-    marks: [
-      Plot.dot(data, {
-        x: "fertility",
-        y: "life_expect",
-        stroke: "#666af0",
-        r: "pop",
-      }),
-    ],
-    x: {
-      domain: [0, d3.max(data, (d) => d.fertility)],
-    },
-    y: {
-      tickFormat: d3.format(".0f"),
-      domain: [0, d3.max(data, (d) => d.life_expect)],
-    },
-    r: {
-      range: [0, 18],
-    },
+    line: true,
+    grid: true,
+    zero: true,
   });
   d3.select("#chart").append(() => chart);
 });
