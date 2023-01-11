d669d9646aef373cf36a611379e3e8a2b71e0648
Carlos Araujo <infocarlosaraujo@gmail.com>
2023-01-11 12:41:03 -0300

    Correcciones intro

--- chart_01_x/script.js
+++ chart_01_x/script.js
@@ -1,12 +1,13 @@
-d3.csv("data2000.csv", d3.autoType).then((data) => {
+d3.csv('data2000.csv', d3.autoType).then(data => {
+  console.log(data)
   // Guardamos el svg generado en la variable chart
   let chart = Plot.plot({
     marks: [
       Plot.dot(data, {
-        x: "fertility",
+        x: 'fertility',
       }),
     ],
-  });
+  })
   // Agregamos chart al div#chart de index.html
-  d3.select("#chart").append(() => chart);
-});
+  d3.select('#chart').append(() => chart)
+})
