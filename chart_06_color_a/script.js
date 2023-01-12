998aa44b8f58638e66278b55046eb53f478d4d50
Carlos Araujo <infocarlosaraujo@gmail.com>
2023-01-12 12:07:51 -0300

    Correccion texto facets

--- chart_06_color_a/script.js
+++ chart_06_color_a/script.js
@@ -1,11 +1,11 @@
-d3.csv("data2000.csv", d3.autoType).then((data) => {
+d3.csv('data2000.csv', d3.autoType).then(data => {
   let chart = Plot.plot({
     marks: [
       Plot.dot(data, {
-        x: "fertility",
-        y: "life_expect",
-        stroke: "cluster",
-        r: "pop",
+        x: 'fertility',
+        y: 'life_expect',
+        stroke: 'cluster',
+        r: 'pop',
       }),
     ],
     grid: true,
@@ -14,7 +14,7 @@ d3.csv("data2000.csv", d3.autoType).then((data) => {
     color: {
       legend: true,
     },
-  });
+  })
 
-  d3.select("#chart").append(() => chart);
-});
+  d3.select('#chart').append(() => chart)
+})
