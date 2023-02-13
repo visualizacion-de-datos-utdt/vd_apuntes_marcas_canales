e35c8527a6b6a6479199514229b7294f481643b8
Carlos Araujo <infocarlosaraujo@gmail.com>
2023-02-13 15:39:01 -0300

    Appendea un titulo a una leyenda con swatches

--- chart_06_color_b/script.js
+++ chart_06_color_b/script.js
@@ -1,11 +1,11 @@
-d3.csv("data2000.csv", d3.autoType).then((data) => {
+d3.csv('data2000.csv', d3.autoType).then(data => {
   let chart = Plot.plot({
     marks: [
       Plot.dot(data, {
-        x: "fertility",
-        y: "life_expect",
-        r: "pop",
-        fill: "cluster",
+        x: 'fertility',
+        y: 'life_expect',
+        r: 'pop',
+        fill: 'cluster',
       }),
     ],
     grid: true,
@@ -13,8 +13,18 @@ d3.csv("data2000.csv", d3.autoType).then((data) => {
     nice: true,
     color: {
       legend: true,
+      className: 'legend-clusters',
     },
-  });
+  })
 
-  d3.select("#chart").append(() => chart);
-});
+  /* Agrega un título a la leyenda x d3 */
+  d3.select(chart)
+    .select('.legend-clusters')
+    .insert('h4', 'span')
+    .attr('class', 'legend-title')
+    .text('Grupos de países: ')
+    .classed('legend-title')
+
+  /* Agrega el chart al DOM */
+  d3.select('#chart').append(() => chart)
+})
